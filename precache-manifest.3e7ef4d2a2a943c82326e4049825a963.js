8197291a47372cc2e61eaa77c13f688ff4973e2d
jacob orellana <jacoborellana97@gmail.com>
2019-11-13 22:33:41 -0600

    Updates

---
+++ precache-manifest.3e7ef4d2a2a943c82326e4049825a963.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b485043b639fb8ec038a80df2413e6f",
+    "url": "/bidlo/index.html"
+  },
+  {
+    "revision": "24214ad37766395a765c",
+    "url": "/bidlo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a5a978f5a4d324ac1c51",
+    "url": "/bidlo/static/js/2.d73ca161.chunk.js"
+  },
+  {
+    "revision": "24214ad37766395a765c",
+    "url": "/bidlo/static/js/main.0f0a7645.chunk.js"
+  },
+  {
+    "revision": "0a5fa5d92a009fb97361",
+    "url": "/bidlo/static/js/runtime-main.ba051a6c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/bidlo/static/media/logo.25bf045c.svg"
+  }
+]);
